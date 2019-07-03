f3ef1bd81b43e8c96b1f7762b2c4272e7d0edbce
Yevgeniy <zleonenkov@gmail.com>
2019-07-03 16:34:06 +0300

    Updates

---
+++ precache-manifest.b075cac48dc304a71ed69e1ad656854a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1c457b4c73732977a521a9da71a0d0b",
+    "url": "/react_static-list-of-posts/index.html"
+  },
+  {
+    "revision": "c6a5e0e0ad04d77cc13a",
+    "url": "/react_static-list-of-posts/static/css/main.615e6684.chunk.css"
+  },
+  {
+    "revision": "0d93b29b27d1d199691f",
+    "url": "/react_static-list-of-posts/static/js/2.95678282.chunk.js"
+  },
+  {
+    "revision": "c6a5e0e0ad04d77cc13a",
+    "url": "/react_static-list-of-posts/static/js/main.50f9a9ce.chunk.js"
+  },
+  {
+    "revision": "3fdea9cdc8e2a1e9a77e",
+    "url": "/react_static-list-of-posts/static/js/runtime~main.c48bb6e1.js"
+  }
+]);
